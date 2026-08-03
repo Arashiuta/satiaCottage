e86924422ae511ed087925c29ab2eb5a47415dc1
Arashiuta <zhishimangguo@will-tec.com>
2026-08-03 10:18:31 +0800

    初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-container">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.home-container {
+  width: 100%;
+  height: 100%;
+
+}
+</style>
